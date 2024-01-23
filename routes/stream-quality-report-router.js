eefc45bd0d512517316b1f5f81940b1bc5bb7e75
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-23 14:51:25 +0800

    add prettierrc

--- routes/stream-quality-report-router.js
+++ routes/stream-quality-report-router.js
@@ -1,37 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+function handleSuccess(res, data) {
+    res.send({
+        errorCode: 0,
+        list: data
+    })
+}
+
+function handleError(res, error) {
+    res.status(500).send({
+        errorCode: 500,
+        message: error
+    })
+}
 
 router.get('/get-all', async (req, res) => {
     try {
-       const data = await req.app.locals.collection.find().toArray();
+        const data = await req.app.locals.collection.find().toArray()
 
-       res.send({
-        errorCode: 0,
-        list: data
-        })
+        handleSuccess(res, data)
     } catch (error) {
-        res.status(500).send({
-            errorCode: 500,
-            message: error
-        })
+        handleError(res, error)
     }
 })
 
-router.get('/get-by-region', async (req, res) => {
+router.get('/get-by-region', async (req, res) => {})
 
-})
+router.get('/get-by-stream-protocol', async (req, res) => {})
 
-router.get('/get-by-stream-protocol', async (req, res) => {
+router.get('/get-by-region-and-stream-protocol', async (req, res) => {})
 
-})
+router.get('/get-by-channel', (req, res) => {})
 
-router.get('/get-by-region-and-stream-protocol', async (req, res) => {
-
-})
-
-router.get('/get-by-channel', (req, res) => {
-
-})
-
-
-module.exports = router;
+module.exports = router
