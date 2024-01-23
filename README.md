d546b8f73ac60e888513762e7a20554083f46dd7
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-23 15:17:40 +0800

    update readme and router rename

--- README.md
+++ README.md
@@ -2,8 +2,40 @@
 
 ## stream-quality-report
 - get-all
+  - query: null
+  - return: Topiq[]
 - get-by-region
-- get-by-stream-protocol
-- get-by-region-and-stream-protocol
+  - query: region
+  - return: Topiq[]
+- get-by-stream-type
+  - query: type
+  - return: Topiq[]
+- get-by-region-and-stream-type
+  - query: region, type
+  - return: Topiq[]
 - get-by-channel
+  - query: channel
+  - return: Topiq[]
+
+
+### structure
+
+- Topiq
+```
+struct Topiq {
+    region: string,
+    type: string,
+    channel: string,
+    topiq_nr: number,
+    topiq_nr-flive: number,
+    topiq_nr-spaq: number
+}
+```
+
+- region: 地區，例如: CEBU。
+- type: 協定類型，例如: RTMP。
+- channel: 桌號，例如: BTCB02。
+- topiq_nr: 待補充。
+- topiq_nr-flive: 待補充。
+- topiq_nr-sppqa: 待補充。
 
--- routes/stream-quality-report-router.js
+++ routes/stream-quality-report-router.js
@@ -25,11 +25,20 @@ router.get('/get-all', async (req, res) => {
     }
 })
 
-router.get('/get-by-region', async (req, res) => {})
+router.get('/get-by-region', async (req, res) => {
+    try {
+        const queryRegion = req.query.region
+        const data = await req.app.locals.collection
+            .find({ region: { $eq: queryRegion } })
+            .toArray()
+    } catch (error) {
+        handleError(res, error)
+    }
+})
 
-router.get('/get-by-stream-protocol', async (req, res) => {})
+router.get('/get-by-stream-type', async (req, res) => {})
 
-router.get('/get-by-region-and-stream-protocol', async (req, res) => {})
+router.get('/get-by-region-and-stream-type', async (req, res) => {})
 
 router.get('/get-by-channel', (req, res) => {})
 
