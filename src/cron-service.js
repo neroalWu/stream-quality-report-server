4c921fbe47fcfca89a3ed14c36792f1294b49ccc
Neroal Wu <neroal.Wu@north.com.tw>
2024-01-31 15:03:05 +0800

    refactor cron service

---
+++ src/cron-service.js
@@ -0,0 +1,23 @@
+const CONFIGURATION = require('./configuration')
+const Logger = require('./util/logger')
+const CronJob = require('cron').CronJob
+class CronService {
+    constructor() {
+        this.logger = new Logger('CronJob')
+    }
+
+    StartJob(callback) {
+        this.logger.Log('Start Job')
+        return new CronJob(CONFIGURATION.CRON_TIME, () => callback(), null, true, 'Asia/Taipei')
+    }
+
+    StopJob(job) {
+        this.logger.Log('Stop Job')
+
+        job && job.stop && job.stop()
+    }
+}
+
+const cronService = new CronService()
+
+module.exports = cronService
